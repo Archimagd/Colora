d46fa4a83612c5163f10b6cc3999090e49e6d67e
Archimagd <qaxqalexandr2009@gmail.com>
2026-01-04 22:37:31 +0400

    Initial commit

---
+++ app/src/main/java/com/example/colora/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.colora;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton new_painting_btn, gallery_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new_painting_btn = findViewById(R.id.new_painting);
+        gallery_btn = findViewById(R.id.gallery);
+
+        new_painting_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewPaintingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        gallery_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GalleryActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
